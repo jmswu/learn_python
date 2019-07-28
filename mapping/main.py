f1684a76a7e4172c7f1b223b8eae8757228b662f
James Wu <jameswu62@yahoo.co.nz>
2019-07-28 21:29:35 +1200

    load data from a file and populate markers

--- mapping/main.py
+++ mapping/main.py
@@ -1,14 +1,25 @@
 import folium
+import pandas
 
+# create map object
 mapObj = folium.Map(location=[-41.2935209, 174.7760023], zoom_start=12)
 
+# extract coordinate data from a CVS file with pandas
+data = pandas.read_csv("Volcanoes.txt")
+names_list = list(data["NAME"])
+lon_list = list(data["LON"])
+lat_list = list(data["LAT"])
+
+# create feature group
 feature_group = folium.FeatureGroup(name="my feature group")
 
-for location in [[-41.2935209, 174.776002], [-41.2925209, 174.775002], [-41.2905209, 174.779002]]:
-    feature_group.add_child(folium.Marker(location=location,
-                                          popup="I am a marker",
+# load file into feature group
+for name, lat, lon in zip(names_list, lat_list, lon_list):
+    feature_group.add_child(folium.Marker(location=[lat, lon],
+                                          popup=name,
                                           icon=folium.Icon(color="blue")))
 
+# add feature group and save a file
 mapObj.add_child(feature_group)
 mapObj.save("map1.html")
 
