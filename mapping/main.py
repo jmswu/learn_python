7c65638736ae3d54e2c8824266c00aee88578739
James Wu <jameswu62@yahoo.co.nz>
2019-07-28 21:53:34 +1200

    add color to different marker

--- mapping/main.py
+++ mapping/main.py
@@ -2,22 +2,32 @@ import folium
 import pandas
 
 # create map object
-mapObj = folium.Map(location=[-41.2935209, 174.7760023], zoom_start=12)
+mapObj = folium.Map(location=[37.2093588, -113.8991741], zoom_start=5)
 
 # extract coordinate data from a CVS file with pandas
 data = pandas.read_csv("Volcanoes.txt")
 names_list = list(data["NAME"])
 lon_list = list(data["LON"])
 lat_list = list(data["LAT"])
+elev_list = list(data["ELEV"])
+
+
+def color_maker(elevation):
+    if elevation < 1500:
+        return "green"
+    elif elevation >= 1500 and elevation < 2500:
+        return "orange"
+    else:
+        return "red"
 
 # create feature group
 feature_group = folium.FeatureGroup(name="my feature group")
 
 # load file into feature group
-for name, lat, lon in zip(names_list, lat_list, lon_list):
+for name, lat, lon, elev in zip(names_list, lat_list, lon_list, elev_list):
     feature_group.add_child(folium.Marker(location=[lat, lon],
-                                          popup=name,
-                                          icon=folium.Icon(color="blue")))
+                                          popup=elev,
+                                          icon=folium.Icon(color=color_maker(elev))))
 
 # add feature group and save a file
 mapObj.add_child(feature_group)
