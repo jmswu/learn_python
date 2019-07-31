363ddacd7b3a2d47c39ab11110c991d630b0a442
James Wu <jameswu62@yahoo.co.nz>
2019-07-31 18:37:38 +1200

    edit test.sh

--- english_dict/test.sh
+++ english_dict/test.sh
@@ -8,3 +8,8 @@ echo "hello" | python3 main.py
 echo "USA" | python3 main.py
 echo "Paris" | python3 main.py
 echo "alala" | python3 main.py
+
+# go to pandas folder
+cd ..
+cd learn_pandas
+python3 main.py
