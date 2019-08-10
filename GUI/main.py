3ada058a0b31cc20bb46ab7e42bde4cf9228c983
James Wu <jameswu62@yahoo.co.nz>
2019-08-10 20:18:51 +1200

    add callback to widget

--- GUI/main.py
+++ GUI/main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
+
+def say_hello():
+    print(e1_value.get())
+    t1.insert(END, e1_value.get())
+
+
 window = Tk()
 
-b1 = Button(window, text="hello")
-b1.pack()
+b1 = Button(window, text="hello1", command=say_hello)
+b1.grid(row=0, column=0)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value)
+e1.grid(row=0, column=1)
+
+t1 = Text(window, height=1, width=50)
+t1.grid(row=0, column=3)
 
 window.mainloop()
